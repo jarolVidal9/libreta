fa80f2f2c71e2aacac9e13afc848386d33cfb0a5
jarolVidal9 <89598568+jarolVidal9@users.noreply.github.com>
2024-01-07 20:23:34 -0500

    login-register mysql2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,9 @@
 const {validateRegister} = require('../schemas/register')
+const {validateLogin} = require('../schemas/login')
 const connection = require('../db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 
 const register = async (req, res) =>{
   try{
@@ -13,8 +15,9 @@ const register = async (req, res) =>{
         //Encript the password
         const hashedPassword = await bcrypt.hash(password, 10);
         //Save in the DB
-        const query ='INSERT INTO users (email, username, name, last_name,gender, date_of_birth, password, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        const values = [email, username, name, last_name, gender, date_of_birth, hashedPassword, image];
+        const id = uuid.v4();
+        const query ='INSERT INTO users (id, email, username, name, last_name,gender, date_of_birth, password, image) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [id, email, username, name, last_name, gender, date_of_birth, hashedPassword, image];
         const consulta = await connection.execute(query, values);
         return res.status(200).json({ message: req.body});
       }else{
@@ -27,9 +30,27 @@ const register = async (req, res) =>{
 }
 const login = (req, res) =>{
   try {
-    
+    const result = validateLogin(req.body)
+    if (result.success){
+      const {email , password} = result.data;
+      const query = 'SELECT * FROM users WHERE email = ?';
+      connection.query(query, [email], async (err, results) => {
+        if (err) return res.status(500).json({ message: 'Error en la base de datos' });
+        if (results.length === 0) return res.status(401).json({ message: 'Credenciales inválidas' });
+        const hashedPassword = results[0].password;
+        // Comparar la contraseña proporcionada con la contraseña almacenada
+        const passwordMatch = await bcrypt.compare(password, hashedPassword);
+        if (!passwordMatch) return res.status(401).json({ message: 'Credenciales inválidas' });
+        // Generar un token JWT
+        const token = jwt.sign({ userId: results[0].id }, 'secreto', { expiresIn: '1h' });
+        res.json({ token });
+      });
+    }else{
+      return res.status(400).json({error: JSON.parse(result.error.message)})
+    }
   } catch (error) {
-    
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
   }
 }
 
--- server/schemas/login.js
+++ server/schemas/login.js
@@ -1,14 +1,16 @@
 const z = require('zod')
 
-
 const loginSchema = z.object({
-    user: z.require({
-        required_error: "Debes ingresar un valor"
-    }).min(3),
-    password: z.require().string().min(5).max(15)
+    email: z.string({ 
+        required_error: "El correo es requerido",
+    }).email({
+        message:"Debe ingresar un correo válido"}
+    ),
+    password: z.string({required_error: "Debes ingresar una contraseña",
+        }).min(5, {message:"La contraseña debe tener al menos 5 caracteres"}).max(20,{message:"La contraseña no debe tener mas de 20 caracteres"}),
 })
 
-function validateLogin(object){
+function validateLogin(Object){
     return loginSchema.safeParse(Object)
 }
 
