32965901430f2f0640dba60bce466c65f8199ec6
Thms <thomasog10@gmail.com>
2024-01-08 12:44:15 -0500

    update-styles-login

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+module.exports = {
+    purge: [],
+    darkMode: false, // Opciones: 'media' o 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
